3fe3170d3c1e188ea360df245be26a1cd11ae889
necanagc <scetcikovoleg@gmail.com>
2025-11-17 18:24:31 +0300

    commit2

--- task12.go
+++ task12.go
@@ -1,5 +1,19 @@
 package main
 
+import "fmt"
+
+type Celsius float64
+
+func (c Celsius) ToFahrenheit() float64 {
+
+	return float64(c)*9/5 + 32
+}
 func main() {
+	var a float64
+	fmt.Scanln(&a)
+
+	b := Celsius(a).ToFahrenheit()
+
+	fmt.Printf("%.2f", b)
 
 }
